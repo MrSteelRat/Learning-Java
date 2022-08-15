e46d5b79cb69566ffa46befd7e6c56de6634c0f8
alex <drebin341@gmail.com>
2022-08-15 04:39:47 -0400

    Merge other class in Main class

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,57 @@ public class Main {
 
         out.flush();
     }
+
+  static class InchMetr {
+      public static void main(String args[]) {
+
+          double sizeInches;
+          double sizeMeter;
+          int counter;
+
+          counter = 0;
+          for (sizeInches = 1; sizeInches <= 100; sizeInches++) {
+              sizeMeter = sizeInches / 39.72;
+              System.out.println(sizeInches + " Inche " + sizeMeter + " Meter ");
+          }
+          counter++;
+          if (counter == 12) {
+              System.out.println();
+              counter = 0;
+          }
+      }
+  }
+
+    static class WeightOnMoon {
+        public static void main(String args[]) {
+            double weightOnEarth = 86;
+            double weightOnMoon = weightOnEarth * 0.17;
+
+            System.out.println("Your weight on the moon is " + weightOnMoon + " kg");
+        }
+    }
+
+    static class GalToLitTable {
+        public static void main(String args[]) {
+            double gallons; //variable for value gallons
+            double liters; // variable for value litres
+            int counter;
+
+            counter = 0;
+            {
+                liters = gallons * 3.7854;
+
+                System.out.println(gallons + " gallons corresponds to" + liters + " liers");
+
+            }
+
+            counter++;
+            if (counter == 10) {
+                {
+                    System.out.println();
+                }
+                counter = 0;
+            }
+        }
+    }
 }
--- src/MassOnMoon.java
+++
@@ -1,8 +0,0 @@
-class WeightOnMoon {
-    public static void main(String args[]) {
-        double weightOnEarth = 86;
-        double weightOnMoon = weightOnEarth * 0.17;
-
-        System.out.println("Your weight on the moon is " + weightOnMoon + " kg");
-    }
-}
