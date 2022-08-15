a15e38b9422a4d04ca082295a28a66eedbd774b6
alex <drebin341@gmail.com>
2022-08-15 04:35:55 -0400

    Ch3 Study 10

--- src/Ch3Study10.java
+++ src/Ch3Study10.java
@@ -1,11 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Ch3Study10 {
-  public  static void main(String[] args){
+  public static void main(String[] args) throws java.io.IOException {
     char ch;
-    int spaceconter_size;
-
-    while (ch != ' ')
-    {
-
+    int case_changes = 0;
+    System.out.println("Press . to stop.");
+    while ((ch = (char) System.in.read()) != '.') {
+      if (ch >= 'a' && ch <= 'z') {
+        System.out.print((char) (ch - 32));
+        case_changes++;
+      } else if (ch >= 'A' && ch <= 'Z') {
+        System.out.print((char) (ch + 32));
+        case_changes++;
+      } else {
+        System.out.print(ch);
+      }
     }
+    System.out.println(" the number of case changes:" + case_changes);
   }
 }
+// aaaAAAAssss6666.
